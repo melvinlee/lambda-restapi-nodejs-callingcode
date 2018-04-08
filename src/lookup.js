8ed1cda6d34488dc98f5c47953169cd20ed2759d
Lee Chin Yong <melvinlee@hotmail.com>
2018-04-08 13:34:11 +0800

    fix: cleanup code.

--- src/lookup.js
+++ src/lookup.js
@@ -35,14 +35,3 @@ module.exports.lookup = (event, context, callback) => {
 
   callback(null, response);
 };
-
-module.exports.list = (event, context, callback) => {
-  console.log(`Received event: ${JSON.stringify(event, null, 2)}`);
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(callingCode.list())
-  };
-
-  callback(null, response);
-};
